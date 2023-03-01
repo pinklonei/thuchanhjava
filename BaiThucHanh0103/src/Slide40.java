8b517e8c6661155b857d5bfab704337d54f2319a
minhnotcode <124772100+minhnotcode@users.noreply.github.com>
2023-03-01 19:52:34 +0700

    commit note

---
+++ BaiThucHanh0103/src/Slide40.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Slide40 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap vao mot so tu 1 den 7: ");
+        int day;
+        day = sc.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("Chu nhat");
+                break;
+            case 2:
+                System.out.println("Thu hai");
+                break;
+            case 3:
+                System.out.println("Thu ba");
+                break;
+            case 4:
+                System.out.println("Thu tu");
+                break;
+            case 5:
+                System.out.println("Thu nam");
+                break;
+            case 6:
+                System.out.println("Thu sau");
+                break;
+            case 7:
+                System.out.println("Thu bay");
+                break;
+            default:
+                System.out.println("So nhap vao khong hop le");
+                break;
+        }
+    }
+}
