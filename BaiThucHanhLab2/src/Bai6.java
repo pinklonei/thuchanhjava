f1d72478eaee029ab9c5ca7aa622567ae2d803e2
minhnotcode <124772100+minhnotcode@users.noreply.github.com>
2023-03-15 09:34:39 +0700

    Lab2

--- BaiThucHanhLab2/src/Bai6.java
+++ BaiThucHanhLab2/src/Bai6.java
@@ -5,10 +5,14 @@ public class Bai6 {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         int n;
+        long giaithua = 1;
         do{
-            System.out.println("Nhap mot so nguyen duong");
+            System.out.print("Nhap mot so nguyen duong ");
             n = sc.nextInt();
         }while(n<=0);
+        for (int i = 1; i <= n; i++) {
+            giaithua *= i;
+        }
+        System.out.println("Giai thua cua " + n + " la: " + giaithua);
     }
-    
 }
